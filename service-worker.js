9493c01a8d7e42c40e0f67e184e9528d2aaa2033
chotudarade95-lab <chotudarade95@gmail.com>
2026-03-15 19:06:21 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+
+const CACHE_NAME = "tminus-cache-v2";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+  if (
+    event.request.url.includes("firebase") ||
+    event.request.url.includes("firestore")
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
