1821178bf680c3f3f280b80f5e4c38dbceb9b5b5
chotudarade95-lab <chotudarade95@gmail.com>
2026-03-17 06:54:23 +0530

    Refactor service worker for caching and request handling
    Updated service worker to manage cache more effectively and exclude specific requests.

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,43 @@
+const CACHE_NAME = "tminus-cache-v3";
 
-const CACHE = "tminus-cache-v1";
-const urlsToCache = ["/", "/index.html"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e => {
+self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
+self.addEventListener("activate", e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key => {
+if(key !== CACHE_NAME){
+return caches.delete(key);
+}
+})
+)
+)
+);
+self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+
+if(
+e.request.url.includes("firebase") ||
+e.request.url.includes("firestore")
+){
+return;
+}
+
+e.respondWith(
+fetch(e.request)
+.then(res=>{
+const clone=res.clone();
+caches.open(CACHE_NAME).then(cache=>{
+cache.put(e.request,clone);
+});
+return res;
+})
+.catch(()=>caches.match(e.request))
+);
+
 });
